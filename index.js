cc815f414ac0e7081439a7470c7fc373d1283f67
Terrell <wrightterrell049@gmail.com>
2022-09-07 14:02:11 -0400

    webspider v1 cbh

--- index.js
+++ index.js
@@ -8,4 +8,33 @@ import { urltoFileName } from './utils.js'
 export function spider (err, url){
     const filename = urltoFileName(url);
     //console.log(filenname)
+    fs.access(filename, err => {
+        if(err && err.code === 'ENOENT') {
+            console.log{`downloing ${url} into ${filename}`}
+            superagent.get(url).end((err, res) =>{
+                if(err){
+                    cb(err)
+                }
+                else {
+                    mkdirp(path.dirname(filename), err => {
+                        if(err){
+                            cb(err)
+                        }
+                        else {
+                            fs.writeFile(filename, res.text, err => {
+                                if (err) {
+                                    cb(err)
+                                } else {
+                                    cb(null, filename, true)
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        } else {
+            cb(null, filename, false)
+        }
+    })
+
 }
